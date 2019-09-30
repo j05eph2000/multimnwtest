58fd543fc526f9589109a4dfe8af23f75022f061
j05eph2000 <47005688+j05eph2000@users.noreply.github.com>
2019-09-30 23:20:57 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -203,7 +203,7 @@ EOF
   systemctl start wagerr_$ALIAS.service
   systemctl enable wagerr_$ALIAS.service >/dev/null 2>&1
   echo -e " Wait for a minute "
-  sleep 90
+  sleep 180
 
   #(crontab -l 2>/dev/null; echo "@reboot sh ~/bin/wagerrd_$ALIAS.sh") | crontab -
 #	   (crontab -l 2>/dev/null; echo "@reboot sh /root/bin/wagerrd_$ALIAS.sh") | crontab -
