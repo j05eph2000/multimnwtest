e779217a70b0b1fc5875da19331c7e76322871f1
j05eph2000 <47005688+j05eph2000@users.noreply.github.com>
2019-12-22 17:51:26 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@ libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev  libdb5.3++ unzip
 
 
 
-fallocate -l 35G /wswapfile
+fallocate -l 12G /wswapfile
 chmod 600 /wswapfile
 mkswap /wswapfile
 swapon /wswapfile
